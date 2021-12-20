42e9577500832031d34b40f18e29d3a743aa7c32
01AnasAhmad <79102789+01AnasAhmad@users.noreply.github.com>
2021-12-20 16:51:30 +0530

    Basic Classes Defined added

---
+++ src/WillHero/Helmet.java
@@ -0,0 +1,12 @@
+package WillHero;
+
+public class Helmet {
+    private String name;
+    private Weapon weapon1;
+    private Weapon weapon2;
+
+    Helmet(){
+
+    }
+
+}
---
+++ src/WillHero/Hero.java
@@ -0,0 +1,15 @@
+package WillHero;
+
+public class Hero extends GameObjects implements Movable{
+    private Helmet helmet;
+    private int weapon_flag;
+    private Platform platform;
+    private double jump_height;
+    private double current_y_position;
+    private double length;
+    private int coins;
+    Hero(){
+
+    }
+
+}
