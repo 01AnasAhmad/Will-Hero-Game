bbadddf26f0fd833b1f6b5319683d17ea939cb58
ishaan20068 <78277847+ishaan20068@users.noreply.github.com>
2021-12-25 13:36:28 +0530

    Update GameObjects.java

--- src/WillHero/GameObjects.java
+++ src/WillHero/GameObjects.java
@@ -1,11 +1,10 @@
 package WillHero;
-
 public class GameObjects implements Serializable{
-    private int unique_id;
     private Location location1;
     private double x_coordinate;
     private double y_coordinate;
-    GameObjects(){
-
+    private Image
+    GameObjects(Location loc,){
+        
     }
 }
